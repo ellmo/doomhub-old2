3d6ab70f6af58523dc1d34a10517d0fc9b4924de
ellmo <ellmunzai@gmail.com>
2014-06-27 15:34:52 +0200

    better project name/url/slug validations

--- app/models/project.rb
+++ app/models/project.rb
@@ -18,16 +18,23 @@ class Project < ActiveRecord::Base
 #============
 
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+  validates :url_name, uniqueness: {case_sensitive: false}
 
 #============
 #= CALLBACKS
 #==========
 
+  before_save :trim_spaces, if: ->(p) {p.new_record? || p.name_changed?}
   before_save :generate_default_url_name, unless: ->(p) {p.url_name.present?}
 
   def generate_default_url_name
     self.url_name = name
     self.send :set_slug
+  end
+
+  def trim_spaces
+    self.name = name.strip.gsub(/\s+/, ' ')
+    self.url_name = url_name.strip.gsub(/\s+/, ' ') if url_name
   end
 
 end
