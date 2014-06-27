491a9fcff4e0b59c89e359667adf1b21149ab3af
ellmo <ellmunzai@gmail.com>
2014-06-27 22:17:57 +0200

    upload class interpolations, validations and tests

--- app/models/upload.rb
+++ app/models/upload.rb
@@ -14,6 +14,47 @@ class Upload < ActiveRecord::Base
   has_attached_file :archive,
                     keep_old_files: true,
                     url: ':path',
-                    path: 'tmp'
+                    path: ENV['PAPERCLIP_UPLOAD_ARCHIVE_PATH']
+  do_not_validate_attachment_file_type :archive
+  validates_attachment_presence :archive
+
+#==============
+#= VALIDATIONS
+#============
+
+  validate :check_attachment
+
+  ALLOWED_MIMES = ["application/zip", "application/x-7z-compressed", "application/x-rar-compressed"]
+
+  Paperclip.interpolates :uploadable_type do |attachment, style|
+    attachment.instance.uploadable_type.downcase
+  end
+
+  Paperclip.interpolates :uploadable_id do |attachment, style|
+    attachment.instance.uploadable_id
+  end
+
+  Paperclip.interpolates :name do |attachment, style|
+    (attachment.instance.name || attachment.instance.archive_file_name).parameterize
+  end
+
+  def check_attachment
+    if (MIME::Types.type_for(archive_file_name).map(&:to_s) & ALLOWED_MIMES).empty?
+      errors[:archive] << "File must be a valid zip / rar / 7z achive"
+    end
+    if archive.size > 1.megabyte
+      errors[:archive] << "File must not be over 1 MB"
+    end
+  end
+
+#============
+#= CALLBACKS
+#==========
+
+  before_save :generate_name, unless: ->(u) {u.name.present?}
+
+  def generate_name
+    self.name = archive_file_name.parameterize
+  end
 
 end
--- test/models/upload_test.rb
+++ test/models/upload_test.rb
@@ -1,13 +1,48 @@
 require "test_helper"
 
-class UploadTest < ActiveSupport::TestCase
+describe Upload do
+  let(:user){ users :ellmo }
+  let(:project){ projects :dtwid }
 
-  def upload
-    @upload ||= Upload.new
+  describe 'create' do
+    it 'is valid for zip files' do
+      upload = project.uploads.build
+      upload.archive = fixture_file_upload('uploads/wadfile.zip')
+      upload.valid?.must_equal true
+    end
+
+    it 'is valid for rar files' do
+      upload = project.uploads.build
+      upload.archive = fixture_file_upload('uploads/wadfile.rar')
+      upload.valid?.must_equal true
+    end
+
+    it 'is valid for 7z files' do
+      upload = project.uploads.build
+      upload.archive = fixture_file_upload('uploads/wadfile.7z')
+      upload.valid?.must_equal true
+    end
+
+    it 'is NOT valid for image files' do
+      upload = project.uploads.build
+
+      upload.archive = fixture_file_upload('uploads/ellmo.png')
+      upload.valid?.must_equal false
+
+      upload.archive = fixture_file_upload('uploads/ellmo.jpg')
+      upload.valid?.must_equal false
+
+      upload.archive = fixture_file_upload('uploads/ellmo.tiff')
+      upload.valid?.must_equal false
+    end
   end
 
-  def test_valid
-    assert upload.valid?
+  describe 'file saving' do
+    it 'peoperly saves file without a name' do
+      upload = project.uploads.build
+      upload.archive = fixture_file_upload('uploads/wadfile.zip')
+      upload.save
+      File.exists?(Rails.root.join("tmp/test-uploads/project/#{project.id}/archives/wadfile-zip-#{upload.id}.zip")).must_equal true
+    end
   end
-
 end
