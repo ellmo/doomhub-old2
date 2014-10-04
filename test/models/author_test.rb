82808ae9b4b6d22cdf140661773c833c6a5e0054
ellmo <ellmunzai@gmail.com>
2014-10-04 23:42:32 +0200

    assertions for Author#login

--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -8,4 +8,14 @@ describe Author do
       user.errors.must_be_empty
     end
   end
+
+  describe '#login' do
+    it 'returns name for login if there`s no user' do
+      authors(:shaun).login.must_equal 'Shaun'
+    end
+
+    it 'refers to User`s login, even if it has a name' do
+      authors(:ellmo).login.must_equal 'ellmo'
+    end
+  end
 end
